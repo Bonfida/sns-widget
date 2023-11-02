4e9a9f9f7b48b1747ae1ac226ff8da0ba7f53ed0
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2023-11-02 22:13:31 +0200

    feat: Add preview build for GitHub pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,63 +1,77 @@
 import dts from "vite-plugin-dts";
 import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import { type UserConfig, defineConfig } from "vite";
 import path from "path";
 import { terser } from "rollup-plugin-terser";
 
 const resolvePath = (str: string) => path.resolve(__dirname, str);
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    port: 3000,
-  },
-  build: {
-    lib: {
-      entry: resolvePath("src/lib/index.tsx"),
-      name: "SNS Widget",
-      formats: ["es", "cjs", "umd"],
-      fileName: (format) =>
-        `sns-widget.${
-          format === "cjs" ? "cjs" : format === "es" ? "mjs" : "umd.js"
-        }`,
-    },
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "react/jsx-runtime",
-        "@pythnetwork/client",
-        "@solana/web3.js",
-        "@solana/spl-token",
-        "ahooks",
-        "tailwind-merge",
-        "buffer",
-        "react-huge-icons",
-        "react-async-hook",
-        "@bonfida/spl-name-service",
-        "split-graphemes",
-      ],
-      output: {
-        // Provide global variables to use in the UMD build for externalized deps
-        globals: {
-          react: "react",
-          "react-dom": "react-dom",
-          "react/jsx-runtime": "react/jsx-runtime",
-          "@pythnetwork/client": "@pythnetwork/client",
-          "@solana/web3.js": "@solana/web3.js",
-          "@solana/spl-token": "@solana/spl-token",
-          "@bonfida/spl-name-service": "@bonfida/spl-name-service",
-          ahooks: "ahooks",
-          bs58: "bs58",
-          "tailwind-merge": "tailwind-merge",
-          buffer: "buffer",
-          "react-huge-icons": "react-huge-icons",
-          "react-async-hook": "react-async-hook",
-          "split-graphemes": "split-graphemes",
-        },
+export default defineConfig(({ mode }) => {
+  const libConfig = {
+    build: {
+      lib: {
+        entry: resolvePath("src/lib/index.tsx"),
+        name: "SNS Widget",
+        formats: ["es", "cjs", "umd"],
+        fileName: (format) =>
+          `sns-widget.${
+            format === "cjs" ? "cjs" : format === "es" ? "mjs" : "umd.js"
+          }`,
       },
-      plugins: [terser({ compress: true })],
+      rollupOptions: {
+        external: [
+          "react",
+          "react-dom",
+          "react/jsx-runtime",
+          "@pythnetwork/client",
+          "@solana/web3.js",
+          "@solana/spl-token",
+          "ahooks",
+          "tailwind-merge",
+          "buffer",
+          "react-huge-icons",
+          "react-async-hook",
+          "@bonfida/spl-name-service",
+          "split-graphemes",
+        ],
+        output: {
+          // Provide global variables to use in the UMD build for externalized deps
+          globals: {
+            react: "react",
+            "react-dom": "react-dom",
+            "react/jsx-runtime": "react/jsx-runtime",
+            "@pythnetwork/client": "@pythnetwork/client",
+            "@solana/web3.js": "@solana/web3.js",
+            "@solana/spl-token": "@solana/spl-token",
+            "@bonfida/spl-name-service": "@bonfida/spl-name-service",
+            ahooks: "ahooks",
+            bs58: "bs58",
+            "tailwind-merge": "tailwind-merge",
+            buffer: "buffer",
+            "react-huge-icons": "react-huge-icons",
+            "react-async-hook": "react-async-hook",
+            "split-graphemes": "split-graphemes",
+          },
+        },
+        plugins: [terser({ compress: true })],
+      },
     },
-  },
-  plugins: [react(), dts({ rollupTypes: true, include: ["src/lib"] })],
+    plugins: [react(), dts({ rollupTypes: true, include: ["src/lib"] })],
+  };
+  const previewConfig = {
+    build: {
+      outDir: "./preview-build",
+    },
+    plugins: [react()],
+  };
+
+  const buildConfig = mode === "lib" ? libConfig : previewConfig;
+
+  return {
+    server: {
+      port: 3000,
+    },
+    ...buildConfig,
+  } as UserConfig;
 });
