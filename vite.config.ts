49e8bd27bc60beb02106449b38c86f3e1b92771c
DR497 <47689875+dr497@users.noreply.github.com>
2023-11-08 19:45:32 +0800

    fix: use correct terser plugin
    `rollup-plugin-terser` is no longer maintained

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import { type UserConfig, defineConfig } from "vite";
 import path from "path";
 import { nodePolyfills } from "vite-plugin-node-polyfills";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 const resolvePath = (str: string) => path.resolve(__dirname, str);
 
