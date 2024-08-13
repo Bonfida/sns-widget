e5569069875a5c5c0091f466bcc7bb1eb70d0fab
Dmytro Svyrydenko <dmytro.svyrydenko@gmail.com>
2024-08-13 18:58:17 +0200

    feat: Add hover effect to buttons

--- src/lib/components/button.tsx
+++ src/lib/components/button.tsx
@@ -23,6 +23,7 @@ export const CustomButton = ({
       tabIndex={0}
       className={twMerge(
         "text-white flex items-center justify-center gap-2 bg-theme-primary px-3 font-primary",
+        "transition-[filter] hover:[filter:brightness(0.9)]",
         size === "full" && "rounded-xl py-2.5 tracking-wide",
         size === "small" && "rounded-lg py-1 text-sm",
         disabled && "bg-theme-secondary",
